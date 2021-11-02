1f72534160ca5d61dc0d1f72e896e4c23590a469
otmane1993 <otmomks@gmail.com>
2021-11-02 18:30:13 +0000

    questionnaire en cours JS

--- App/script.js
+++ App/script.js
@@ -12,6 +12,58 @@ suivant.addEventListener("click",function(){
     {
         i=Tab.length-1;
     }
+    if(i==2)
+    {
+        form.innerHTML=`<label class="main__form--label" for="temp">Température:</label>
+        <input type="number" placeholder="37" class="main__form--input" id="temp" name="temp">`;
+    }
+    else if(i==11)
+    {
+        form.innerHTML=`
+        <div class="form-group">
+                <input class="main__form--input" type="radio" name="choix" value="bien" id="bien">
+                <label class="main__form--label" for="bien">Bien</label>
+                
+            </div>
+            <div class="form-group">
+                <input class="main__form--input" type="radio" name="choix" value="assez" id="assez">
+                <label class="main__form--label" for="assez">Assz bien</label>
+                
+            </div>
+            <div class="form-group">
+                <input class="main__form--input" type="radio" name="choix" value="mal" id="mal">
+                <label class="main__form--label" for="mal">mal</label>
+                
+            </div>
+            <div class="form-group">
+                <input class="main__form--input" type="radio" name="choix" value="tres-mal" id="tres-mal">
+                <label class="main__form--label" for="tres-mal">tres mal</label>
+        `;
+    }    
+        else if(i==12)
+        {
+            form.innerHTML=`<label class="main__form--label" for="symp">Symptomes:</label>
+            <input type="text" placeholder="symptomes" class="main__form--input" id="symp" name="symp">`;
+        }
+        else if(i==13)
+        {
+            form.innerHTML=`<div class="form-group">
+                <input class="main__form--input" type="radio" name="choix" value="moins-50" id="moins-50">
+                <label class="main__form--label" for="moins-50">moins de 50ans</label>
+            </div>
+            <div class="form-group">
+                <input class="main__form--input" type="radio" name="choix" value="50-69" id="50-69">
+                <label class="main__form--label" for="50-69">50-69ans</label>
+            </div>
+            <div class="form-group">
+                <input class="main__form--input" type="radio" name="choix" value="sup-70" id="sup-70">
+                <label class="main__form--label" for="sup-70">supérieur à 70 ans</label>
+            </div>
+            <div class="form-group">
+                <input class="main__form--input" type="radio" name="choix" value="moins-15" id="moins-15">
+                <label class="main__form--label" for="moins-15">moins de 15 ans</label>`
+        }
+    
     form.classList.add(`.main__form${i}`);
     form.classList.remove(`.main__form${i-1}`);
     progress.style.width=`${(i*(100/23))}%`;
