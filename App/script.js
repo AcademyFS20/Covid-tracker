aa44c280b33360c40fea2bef106d76f2e651055f
otmane1993 <otmomks@gmail.com>
2021-11-02 19:20:40 +0000

    questionnaire terminé

--- App/script.js
+++ App/script.js
@@ -4,8 +4,8 @@ const title=document.querySelector(".main__title");
 const precedent=document.querySelector(".lien-prec");
 const span=document.querySelector(".progressbar__span");
 const suivant=document.querySelector(".lien-suiv");
-const Tab=["","Pensez-vous avoir eu de la fièvre ces derniers jours (frissons, sueurs) ?","Si oui : quelle est votre température ?","Avez-vous une toux ou une augmentation de votre toux habituelle ces derniers jours ?","Avez-vous des douleurs musculaires ou des courbatures inhabituelles ces derniers jours?","Avez-vous un mal de gorge apparu ces derniers jours ?","Avez-vous de la diarrhée ces dernières 24 heures (au moins 3 selles molles) ? ","Avez-vous une fatigue inhabituelle ces derniers jours ? ","Si oui cette fatigue vous oblige-t-elle à vous reposer plus de la moitié de la journée ? ","Avez-vous des difficultés importantes pour vous alimenter ou boire depuis plus de 24h?","Avez-vous vu apparaître une gêne respiratoire ou une augmentation de votre gêne respiratoire habituelle ? ","Comment vous sentez-vous ? ","Avez-vous d’autre symptôme ? ","Quel est votre âge ?","Quel est votre poids ? Quelle est votre taille ?","Avez-vous de l’hypertension artérielle ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez vous un traitement à visée cardiologique ? ","Êtes-vous diabétique ? ","Avez-vous ou avez-vous eu un cancer ? ","Avez-vous une maladie respiratoire ? Ou êtes-vous suivi par un pneumologue ? ","Avez-vous une insuffisance rénale chronique dialysée ?","Avez-vous une maladie chronique du foie ? ","Êtes-vous enceinte ? ","●	Avez-vous une maladie connue pour diminuer vos défenses immunitaires ","Prenez-vous un traitement immunosuppresseur ?"];
-let i=0;
+const Tab=["","Pensez-vous avoir eu de la fièvre ces derniers jours (frissons, sueurs) ?","Si oui : quelle est votre température ?","Avez-vous une toux ou une augmentation de votre toux habituelle ces derniers jours ?","Avez-vous des douleurs musculaires ou des courbatures inhabituelles ces derniers jours?","Avez-vous un mal de gorge apparu ces derniers jours ?","Avez-vous de la diarrhée ces dernières 24 heures (au moins 3 selles molles) ? ","Avez-vous une fatigue inhabituelle ces derniers jours ? ","Si oui cette fatigue vous oblige-t-elle à vous reposer plus de la moitié de la journée ? ","Avez-vous des difficultés importantes pour vous alimenter ou boire depuis plus de 24h?","Avez-vous vu apparaître une gêne respiratoire ou une augmentation de votre gêne respiratoire habituelle ? ","Comment vous sentez-vous ? ","Avez-vous d’autre symptôme ? ","Quel est votre âge ?","Quel est votre poids ? Quelle est votre taille ?","Avez-vous de l’hypertension artérielle ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez vous un traitement à visée cardiologique ? ","Êtes-vous diabétique ? ","Avez-vous ou avez-vous eu un cancer ? ","Avez-vous une maladie respiratoire ? Ou êtes-vous suivi par un pneumologue ? ","Avez-vous une insuffisance rénale chronique dialysée ?","Avez-vous une maladie chronique du foie ? ","Êtes-vous enceinte ? ","Avez-vous une maladie connue pour diminuer vos défenses immunitaires ","Prenez-vous un traitement immunosuppresseur ?"];
+let i=1;
 suivant.addEventListener("click",function(){
     i++;
     if(i>Tab.length-1)
@@ -264,5 +264,18 @@ precedent.addEventListener("click",function(){
 })
 window.addEventListener("load",function(){
     span.innerText=1;
+    title.innerText=Tab[1];
+    form.classList.add(`.main__form${1}`);
+    form.innerHTML=`
+    <div class="form-group">
+        <input class="main__form--input" type="radio" name="choix" value="oui" id="oui">
+        <label class="main__form--label" for="oui">Oui</label>
+    </div>
+    <div class="form-group">
+        <input class="main__form--input" type="radio" name="choix" value="non" id="non">
+        <label class="main__form--label" for="non">non</label>
+        
+    </div>
+    `;
 })
 
