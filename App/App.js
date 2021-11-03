2cf66529bee17a08105ef9698522c022430cf960
oussama achemlal <oussama.achemlal7@gmail.com>
2021-11-03 11:02:49 +0100

    api second commit

--- App/App.js
+++ App/App.js
@@ -101,44 +101,86 @@ function datesCases(data){
     console.log(numberCases(dataMoroccoLast15Days));
     console.log(datesCases(dataMoroccoLast15Days));
 
-    const config = {
+const config = {
         type: 'bar',
         data: {
                 labels: datesCases(dataMoroccoLast15Days),
-                datasets: [{ label: 'Morocco COVID19 information', 
+                datasets: [{ label: 'Case numbers', 
                             data: numberCases(dataMoroccoLast15Days)}],
                 backgroundColor: [
-                    'rgb(255,159,64)', //orange
-                    'rgb(255,99,132)', //red
-                    'rgb(54,162,235)', //blue
-                    'rgb(75,192,192)', //green
-                    'rgb(153,102,255)', //purple
-                    'rgb(255, 99, 132)',
-                    'rgb(54, 162, 235)',
-                    'rgb(255, 205, 86)',
-                    'rgb(255, 105, 86)',
-                    'rgb(255, 5, 86)',
-                    'rgb(255, 99, 132)',
-                    'rgb(255, 159, 64)',
-                    'rgb(255, 205, 86)',
-                    'rgb(255, 99, 132)',
-                    'rgb(255, 159, 64)'
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 205, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(201, 203, 207, 0.2)',
+                    'rgba(255, 80, 132, 0.2)',
+                    'rgba(255, 50, 64, 0.2)',
+                    'rgba(255, 70, 86, 0.2)',
+                    'rgba(75, 66, 192, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(153, 102, 110, 0.2)',
+                    'rgba(201, 203, 1, 0.2)',
+                    'rgba(201, 2500, 20, 0.2)'
 
-                ],
-                borderColor: [
-                    'rgb(255, 99, 132)',
-                    'rgb(255, 159, 64)',
-                    'rgb(255, 205, 86)',
-                    'rgb(75, 192, 192)',
-                    'rgb(54, 162, 235)',
-                    'rgb(153, 102, 255)',
-                    'rgb(201, 203, 207)'
-                  ],
+
+                ]
+                ,
+                
+                borderColor: '#777',
                   
-                  borderWidth: 1
+                borderWidth: 1,
+                hoveredBorderColor: '#000',
+                hoverBorderWidth:2,
+                options:{
+                    title:{
+                        display:true,
+                        text:'Morocco Covid 19 information'
+                    }
                 }
-              };
+            }
+                
+        };
 
-      const myBarChart = new Chart(ctxBar,config)
-    }
-    
+    const myBarChart = new Chart(ctxBar,config)
+}
+
+
+async function drawLineChart(url){
+    const data = await getData(url)
+    const totalConfirmed = data.Global.TotalConfirmed
+    const totalDeaths = data.Global.TotalDeaths
+    const countries = data.Global.Country
+
+    const dataShart = {
+        labels:[
+            'nouveaux cas confirmés',
+            'total cas cnfirmés',
+            'nouveau décès',
+            'total des décès',
+            'total rétablis'
+        ],
+        datasets: [{
+            label: 'World Summary',
+            data: [newConfirmed, totalConfirmed, newDeaths,totalDeaths,totalRecovered],
+            backgroundColor: [
+              'rgb(255, 99, 132)',
+              'rgb(54, 162, 235)',
+              'rgb(255, 205, 86)',
+              'rgb(255, 105, 86)',
+              'rgb(255, 5, 86)'
+            ],
+            hoverOffset: 4
+          }]
+        };
+
+    const config = {
+        type: 'doughnut',
+        data: dataShart,
+      };
+
+   const myChart = new Chart(ctx,config)
+
+
+}
